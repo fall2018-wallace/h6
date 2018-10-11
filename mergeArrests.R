c099e1b5fef01909910f8ce77ecbd4d917225b45
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 17:27:32 -0400

    Saving mergeArrests.R

--- mergeArrests.R
+++ mergeArrests.R
@@ -3,6 +3,7 @@
  localUSArrests
 
 df
+df<-df[-9,]
 row.names(localUSArrests)     #Checking if the row names are stateNames, which they are not. 
 #We will have to create a column with these names so that we have a common column to merge the dataframes by.
 
